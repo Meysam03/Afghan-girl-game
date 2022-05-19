63f881f33013b1e665f17f17093ff04b63f0c161
Meysam03 <alre03001@utb.vaxjo.se>
2022-05-19 22:53:08 +0200

    Spawn
    Jag har lagt backgrunds bild så och skrivit i c# att kamera ska forsätta framåt. Jag har också lagt rigidbody och box collider till mina spel objekter. Jag har lagt bomb objekt och den spawnar random i x och y led.

---
+++ Assets/Script/bomb.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class bomb : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.tag == "border")
+        {
+            Destroy(this.gameObject);
+        }
+    }
+
+}
